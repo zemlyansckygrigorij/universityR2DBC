67c957a4f4b7034b0e75d70cd70c3da1fee9c336
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-07-22 15:53:27 +0500

    create run class

---
+++ src/main/java/com/example/universityR2DBC/UniversityR2DbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.universityR2DBC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityR2DbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityR2DbcApplication.class, args);
+	}
+
+}
