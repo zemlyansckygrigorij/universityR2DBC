7492c6f266b07c69fa3239e71bc77ea6d767658a
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2022-07-22 15:53:44 +0500

    create run test  class

---
+++ src/test/java/com/example/universityR2DBC/UniversityR2DbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.universityR2DBC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityR2DbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
